490e9135ae96e90a11572d13a5913f34b0ab1a48
Florencia Ferrari <ferrariflorenciab@gmail.com>
2021-08-23 12:43:52 -0300

    css

--- portfolio/src/pages/AboutUs.jsx
+++ portfolio/src/pages/AboutUs.jsx
@@ -2,12 +2,14 @@ import React from "react";
 //Import Components
 import AboutSection from "../components/AboutSection";
 import ServicesSection from "../components/ServicesSection";
+import FaqSection from "../components/FaqSection";
 
 export default function AboutUs() {
     return (
         <>
             <AboutSection />
             <ServicesSection />
+            <FaqSection />
         </>
     );
 };
