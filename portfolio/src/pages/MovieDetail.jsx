c2616bce14b3f102cc325c484674e6d9fe838a70
Florencia Ferrari <ferrariflorenciab@gmail.com>
2021-08-28 23:43:17 -0300

    MovieDetail2

--- portfolio/src/pages/MovieDetail.jsx
+++ portfolio/src/pages/MovieDetail.jsx
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import { useHistory } from "react-router-dom";
+import { MovieState } from "../movieState";
 
 export default function MovieDetail() {
     const history = useHistory();
+    const url = history.location.pathname;
+    const [movies, setMovies] = useState(MovieState);
+    const [movie, setMovie] = useState(null);
+
+    useEffect(() => {
+        const currentMovie = movies.filter((stateMovie) => stateMovie.url === url)
+        setMovie(currentMovie);
+    }, [movies, url]);
+
     return (
         <div>
             <h1>MovieDetail</h1>
